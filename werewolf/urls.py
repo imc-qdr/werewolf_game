05563ba53cb4117aa6a07edcb964b7fe4636c979
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2020-11-06 22:08:58 +0100

    Update urls.py

--- werewolf/urls.py
+++ werewolf/urls.py
@@ -18,16 +18,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls, name = 'admin'),
     path('players/', views.getPlayers, name = 'getPlayers'),
     path('roles/', views.assignRoles, name = 'roles'),
     path('firstday', views.day1, name = 'day1'),
+    path('seer', views.seer, name = 'seer'),
+    path('seer1', views.seer1, name = 'seer1'),
+    path('sleepwalk', views.sleepwalking, name = 'sleepwalking'),
+    path('sleepwalk1', views.sleepwalking1, name = 'sleepwalking1'),
+    path('sleepwalk2', views.sleepwalking2, name = 'sleepwalking2'),
     path('night', views.night, name = 'night_template'),
-    path('day', views.day, name = 'day_template')
+    path('witch', views.witch, name = 'witch'),
+    path('white', views.whiteWerewolf, name = 'whiteWerewolf'),
+    path('day', views.day, name = 'day_template'),
+    path('humans_win', views.humans_win, name = 'humans_win'),
+    path('werewolves_win', views.werewolves_win, name = 'werewolves_win'),
+    path('hunter', views.hunter, name = 'hunter')
 ]
-
-urlpatterns += staticfiles_urlpatterns()
-
