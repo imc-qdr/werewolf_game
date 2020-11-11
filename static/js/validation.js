8d192862e4257dde9774ccf8353a715eb3dca42b
31i731 <yougod871@gmail.com>
2020-11-11 09:10:57 +0000

    added comments

---
+++ static/js/validation.js
@@ -0,0 +1,9 @@
+function validateForm()
+{
+    var getPlayer = document.forms["PlayerForm"]["Pname"].value;
+    if (getPlayer === "")
+        {
+            alert('Please input a Name');
+            return false;
+        } 
+}
